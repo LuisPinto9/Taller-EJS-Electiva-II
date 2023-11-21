49cfe0a9889e8042fec73b1e70e98545ab9eecf2
DianaDiazDS <93956532+DianaDiazDS@users.noreply.github.com>
2023-11-21 15:22:37 -0500

    scrip

--- public/js/script.js
+++ public/js/script.js
@@ -14,3 +14,47 @@ function drop(button) {
     };
     xhr.send(`id=${id}`);
 }
+
+// function add() {
+//   const id = document.getElementById("id").value;
+//   const name = document.getElementById("nombre").value;
+//   const type = document.getElementById("tipo").value;
+//   const price = document.getElementById("precio").value;
+
+//   if ((id != "") & (name != "") & (type != "") & (price != "")) {
+//     const xhr = new XMLHttpRequest();
+//     xhr.open("POST", "/", true);
+//     xhr.setRequestHeader(
+//       "Content-Type",
+//       "application/x-www-form-urlencoded",
+//       true
+//     );
+//     xhr.onreadystatechange = () => {
+//       if ((xhr.readyState == 4) & (xhr.status == 200)) {
+//         console.log(xhr.responseText);
+//       }
+//     };
+//     xhr.send(id=${id}&name=${name}&type=${type}&price=${price});
+
+//     document.getElementById("id").value = "";
+//     document.getElementById("nombre").value = "";
+//     document.getElementById("tipo").value = "";
+//     document.getElementById("precio").value = "";
+//     window.location.href = "/";
+//   } else {
+//     alert("Uno o varios campos están vacios");
+//   }
+// }
+
+function update() {
+}
+
+
+
+
+
+
+
+
+
+
