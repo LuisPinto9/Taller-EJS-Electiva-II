99b4ed449c4b69b7d11d7481150211a6a9a1dddb
DianaDiazDS <93956532+DianaDiazDS@users.noreply.github.com>
2023-11-24 17:34:01 -0500

    validacion

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,5 @@
+
+
 function drop(button) {
   const id = button.getAttribute("data-id");
   const xhr = new XMLHttpRequest();
@@ -14,6 +16,39 @@ function drop(button) {
   xhr.send();
 }
 
+//  function add() {
+//   const id = document.getElementById("modalId").value;
+//   const nombre = document.getElementById("modalNombre").value;
+//   const marca = document.getElementById("modalMarca").value;
+//   const cantidad = document.getElementById("modalCantidad").value;
+//   const costo = document.getElementById("modalCosto").value;
+
+//   if (id && nombre && marca && cantidad && costo) {
+//     const xhr = new XMLHttpRequest();
+//     xhr.open("POST", "/stock", true);
+//     xhr.setRequestHeader("Content-Type", "application/json");
+
+//     xhr.onreadystatechange = () => {
+//       if (xhr.readyState == 4 && xhr.status == 200) {
+//         window.location.href = "/stock";
+//       }
+//     };
+
+//     const data = {
+//       id: id,
+//       nombre: nombre,
+//       marca: marca,
+//       cantidad: cantidad,
+//       costo: costo,
+//     };
+
+//     xhr.send(JSON.stringify(data));
+//   } else {
+//     alert("Uno o varios campos están vacíos");
+//   }
+// }
+
+
 function add() {
   const id = document.getElementById("modalId").value;
   const nombre = document.getElementById("modalNombre").value;
@@ -21,13 +56,16 @@ function add() {
   const cantidad = document.getElementById("modalCantidad").value;
   const costo = document.getElementById("modalCosto").value;
 
-  if (id && nombre && marca && cantidad && costo) {
+  const errors = validateInputs(id, nombre, marca, cantidad, costo);
+
+  if (Object.values(errors).every((error) => !error)) {
     const xhr = new XMLHttpRequest();
     xhr.open("POST", "/stock", true);
     xhr.setRequestHeader("Content-Type", "application/json");
 
     xhr.onreadystatechange = () => {
       if (xhr.readyState == 4 && xhr.status == 200) {
+        console.log(xhr.responseText);
         window.location.href = "/stock";
       }
     };
@@ -42,8 +80,26 @@ function add() {
 
     xhr.send(JSON.stringify(data));
   } else {
-    alert("Uno o varios campos están vacíos");
+    alert("Por favor, completa todos los campos correctamente antes de confirmar.");
   }
+}
+
+
+function validateInputs(id, nombre, marca, cantidad, costo) {
+  const idPattern = /^\d+$/;
+  const nombrePattern = /^[A-Za-z\s]+$/;
+  const cantidadPattern = /^\d+$/;
+  const costoPattern = /^\d+(\.\d{1,2})?$/;
+
+  const errors = {
+    id: !idPattern.test(id),
+    nombre: !nombrePattern.test(nombre),
+    marca: !nombrePattern.test(marca),
+    cantidad: !cantidadPattern.test(cantidad),
+    costo: !costoPattern.test(costo),
+  };
+
+  return errors;
 }
 
 function edit(id2) {
